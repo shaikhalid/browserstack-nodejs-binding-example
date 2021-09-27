01c9420d629f0b0fb6883c1a2c1fb7789fb06389
shaikhalid <shaikhalidwork@gmail.com>
2021-09-27 16:24:30 +0530

    synced

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var browserstack = require('browserstack-local');
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const http = require('http');
+
 
 //creates an instance of Local
 var bs_local = new browserstack.Local();
@@ -9,44 +12,44 @@ var bs_local_args = { 'key': process.env.BROWSERSTACK_ACCESS_KEY };
 // starts the Local instance with the required arguments
 bs_local.start(bs_local_args, function() {
   console.log("Started BrowserStackLocal");
+  console.log(bs_local.isRunning());
+  const BROWSERSTACK_USERNAME = process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME';
+    const BROWSERSTACK_ACCESS_KEY = process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY';
+
+    let HttpAgent = new http.Agent({
+        keepAlive: true,
+    });
+
+    let capabilities = {
+        browserName: 'Firefox',
+        name: 'Firefox Test',
+        os: 'Windows',
+        build: 'Test Build 01',
+        project: 'My Awesome App',
+        'browserstack.debug': true,
+        "browserstack.local" : true,
+    };
+
+    let driver = new Builder()
+        .usingHttpAgent(HttpAgent)
+        .withCapabilities(capabilities)
+        .usingServer(`http://${BROWSERSTACK_USERNAME}:${BROWSERSTACK_ACCESS_KEY}@hub-cloud.browserstack.com/wd/hub`)
+        .build();
+
+    driver.get('localhost:8000').then(() => {
+        bs_local.stop(function() {
+            console.log("Stopped BrowserStackLocal");
+        });
+        driver.quit();
+                
+    });
+
 });
 
 // check if BrowserStack local instance is running
-console.log(bs_local.isRunning());
 
-const { Builder, By, Key, until } = require('selenium-webdriver');
-const http = require('http');
 
-const BROWSERSTACK_USERNAME = process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME';
-const BROWSERSTACK_ACCESS_KEY = process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACCESS_KEY';
 
-let HttpAgent = new http.Agent({
-	keepAlive: true,
-});
-
-let capabilities = {
-	browserName: 'Firefox',
-	name: 'Firefox Test',
-	os: 'Windows',
-	build: 'Test Build 01',
-	project: 'My Awesome App',
-	'browserstack.debug': true,
-    "browserstack.local" : true,
-};
-
-let driver = new Builder()
-	.usingHttpAgent(HttpAgent)
-	.withCapabilities(capabilities)
-	.usingServer(`http://${BROWSERSTACK_USERNAME}:${BROWSERSTACK_ACCESS_KEY}@hub-cloud.browserstack.com/wd/hub`)
-	.build();
-
-driver.get('localhost:8000').then(() => {
-	bs_local.stop(function() {
-        console.log("Stopped BrowserStackLocal");
-    });
-	driver.quit();
-			
-});
 
 
 // stop the Local instance
